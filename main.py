8d35096213cc4e5c25d898a9cdf00944f8045cb4
Araujo Dev <silvabaraujo2007@gmail.com>
2026-08-18 18:01:02 -0300

    Início do Projeto Calculadora

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-x = "Oi Kaio"
-y = "Oi Pikachu"
+from tkinter import *
+from tkinter import ttk
 
-print(x)
-print(y)
+black = "#080808"
+grey = "#2e2b2a"
+orange = "#ba3c06"
+
+window = Tk()
+
+window.title("Calculadora")
+window.geometry("235x318")
+window.config(bg=black)
+
+frame_window = Frame(window, width=235, height=50, bg=grey)
+frame_window.grid(row=0, column=0)
+
+second_frame = Frame(window, width=235, height=268, bg=orange)
+second_frame.grid(row=1, column=0)
+window.mainloop()
